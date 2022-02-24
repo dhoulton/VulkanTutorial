8a24f3aee1aa616074c9aaa2f03117ea6651fa10
Dave Houlton <dhoulton@outlook.com>
2022-02-24 14:44:14 -0700

    window create/destroy

--- GameLoop.cpp
+++ GameLoop.cpp
@@ -1,38 +1,67 @@
-#include <vulkan/vulkan.h>
+//#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
 
-class HelloTriangleApplication {
+class HelloTriangleApplication 
+{
 public:
-    void run() {
+    void run() 
+    {
+        initWindow();
         initVulkan();
         mainLoop();
         cleanup();
     }
 
 private:
-    void initVulkan() {
+    void initWindow() 
+    {
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(window_width, window_height, "Vulkan", nullptr, nullptr);
+    }
+
+    void initVulkan() 
+    {
 
     }
 
-    void mainLoop() {
-
+    void mainLoop() 
+    {
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+        }
     }
 
-    void cleanup() {
-
+    void cleanup() 
+    {
+        glfwDestroyWindow(window);
+        glfwTerminate();
     }
+
+private:
+    const uint32_t window_width = 1200;
+    const uint32_t window_height = 900;
+    GLFWwindow* window;
 };
 
-int main() {
+int main() 
+{
     HelloTriangleApplication app;
 
-    try {
+    try 
+    {
         app.run();
     }
-    catch (const std::exception& e) {
+    catch (const std::exception& e) 
+    {
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
     }
